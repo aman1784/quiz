5b5691053cfa8ff602357b70b5b17c32e0bfcf84
Aman Seth <68745555+aman1784@users.noreply.github.com>
2023-05-04 20:46:21 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,102 @@
+let quiz = [
+    {
+        question: "What does the abbreviation HTML stand for?",
+        option: [
+            "HyperText Markup Language",
+            "HighText Markup Language",
+            "HyperText Markdown Language",
+            "None",
+        ],
+        answer: 1,
+    },
+    {
+        question: "How many sizes of headers are available in HTML by default?",
+        option: [
+            "5",
+            "4",
+            "6",
+            "2",
+        ],
+        answer: 3,
+    },
+    {
+        question: "What is the smallest header in HTML by default?",
+        option: [
+            "h1",
+            "h3",
+            "h2",
+            "h6",
+        ],
+        answer: 4,
+    },
+    {
+        question: "How to create an ordered list in HTML?",
+        option: [
+            "<ul>",
+            "<ol>",
+            "<href>",
+            "<b>",
+        ],
+        answer: 2,
+    },
+    {
+        question: "Which attribute is used to provide a unique name to an HTML element?",
+        option: [
+            "class",
+            "id",
+            "None",
+            "type",
+        ],
+        answer: 2,
+    },
+    {
+        question: "What is the correct syntax to write an HTML comment?",
+        option: [
+            "<!-- Comment -->",
+            "// Comment",
+            "# Comment",
+            "/* Comment */",
+        ],
+        answer: 1,
+    },
+    {
+        question: "Which of the following is used to read an HTML page and render it?",
+        option: [
+            "Web server",
+            "Web network",
+            "Web browser",
+            "Web matrix",
+        ],
+        answer: 3,
+    },
+    {
+        question: "What is the correct syntax of doctype in HTML5?",
+        option: [
+            "</doctype html>",
+            "<!doctype html>",
+            "<doctype html>",
+            "<octype html!>",
+        ],
+        answer: 2,
+    },
+    {
+        question: "Which HTML tag is used to set up a Javascript-like client-side scripting language?",
+        option: [
+            "<script>",
+            "<select>",
+            "<anchor>",
+            "<link>",
+        ],
+        answer: 1,
+    },
+    {
+        question: "The most basic part of any HTML page is?",
+        option: [
+            "ASCII Text",
+            "Binary Text",
+            "Text",
+            "None",
+        ],
+        answer: 1,
+    }
+]
---
+++ script.js
@@ -0,0 +1,109 @@
+let index = 0;
+let attempt = 0;
+let score = 0;
+let wrong = 0;
+
+let questions = quiz.sort(function(){
+    return (0.5 - Math.random());
+});
+
+let totalQuestion = questions.length;
+
+$(function () {
+    // timer code
+    let totalTime = 300; 
+    let min = 0;
+    let sec = 0;
+    let counter = 0;
+    
+    let timer = setInterval(function () {
+        counter++;
+        min = Math.floor( (totalTime - counter) / 60);
+        sec = totalTime - min * 60 - counter;
+
+        $(".timerBox span").text(min + ":" + sec);
+
+        if(counter == totalTime){
+            alert("Time's up");
+            result();
+            clearInterval(timer);
+        }
+        
+    }, 1000); // timer set for 1 second interval
+    // timer code
+
+    printQuestion(index);
+});
+
+// Function to print question
+function printQuestion(i) {
+    $(".questionBox").text(questions[i].question);
+    $(".optionBox span").eq(0).text(questions[i].option[0])
+    $(".optionBox span").eq(1).text(questions[i].option[1])
+    $(".optionBox span").eq(2).text(questions[i].option[2])
+    $(".optionBox span").eq(3).text(questions[i].option[3])
+}
+// Function to print question
+
+
+// Function to check answer
+function checkAnswer(option) {
+    attempt++;
+    let optionClicked = $(option).data("opt");
+
+    if(optionClicked == questions[index].answer){
+        $(option).addClass("right");
+        score++;
+    }
+
+    else{
+        $(option).addClass("wrong");
+        wrong++;
+    }
+
+    $(".scoreBox span").text(score);
+
+    $(".optionBox span").attr("onclick", "");
+}   
+// Function to check answer
+
+
+// Function For next question
+function showNext() {
+
+    if(index >= (questions.length - 1)){
+        showResult(0);
+        return;
+    }
+
+    index++;
+    
+    $(".optionBox span").removeClass();
+
+    $(".optionBox span").attr("onclick", "checkAnswer(this)");
+    
+    printQuestion(index);
+}
+// Function For next question
+
+// Function for result
+function showResult(j){
+
+    if(j == 1 && index < (questions.length - 1) && !confirm("Quiz not finished yet, Are you sure to finish anyway?") )
+        return;
+
+    result();
+}
+// Function for result
+
+// Result function
+function result(){
+    $("#questionScreen").hide();
+    $("#resultScreen").show();
+
+    $("#totalQuestion").text(totalQuestion);
+    $("#attemptQuestion").text(attempt);
+    $("#correctAnswers").text(score);
+    $("#wrongAnswers").text(wrong);
+}
+// Result function
